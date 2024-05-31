640ca9f2dc3130ddcb2dbb3a90364c0df8d3e1c1
ShaCode256 <luzonshavit00@gmail.com>
2024-05-31 17:19:38 +0300

    Added files for a new Angular project- Following Envato Tuts+ tutorial.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,23 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { WishItem } from '../shared/models/wishItem';
+import { CommonModule } from '@angular/common'
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
+  items = [
+    new WishItem('Learn Angular'),
+    new WishItem('Drink some coffee', true),
+    new WishItem('Shower'),
+    new WishItem('Grab laundry'),
+    new WishItem('Say a prayer')
+  ];
   title = 'wishlist';
 }
