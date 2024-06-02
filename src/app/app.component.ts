b61c837b1b1021634feeb825080f4e241186aba8
ShaCode256 <luzonshavit00@gmail.com>
2024-06-02 21:51:05 +0300

    Added wishes filter. got up to 3.1 in tutorial.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,26 +2,45 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { WishItem } from '../shared/models/wishItem';
 import { CommonModule } from '@angular/common'
+import { FormsModule } from '@angular/forms';
 
+const filters = [
+  (item: WishItem) => item,
+  (item: WishItem) => !item.isComplete,
+  (item: WishItem) => item.isComplete
+]
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, CommonModule],
+  imports: [RouterOutlet, CommonModule, FormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  items : WishItem[] = [
+  items: WishItem[] = [
     new WishItem('Learn Angular'),
     new WishItem('Drink some coffee', true),
     new WishItem('Shower'),
     new WishItem('Grab laundry'),
     new WishItem('Say a prayer')
   ];
+
+  listFilter: any = '0';
+
+  get visibleItems(): WishItem[] {
+    return this.items.filter(filters[this.listFilter])
+  };
+
+  newWishText = '';
   title = 'wishlist';
 
-  toggleItem(item : WishItem){
+  addNewWish() {
+    this.items.push(new WishItem(this.newWishText))// add wish to items array
+    this.newWishText = ''; // clear the textbox
+  }
+
+  toggleItem(item: WishItem) {
     item.isComplete = !item.isComplete;
     console.log(item);
   }
