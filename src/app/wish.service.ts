e25837d7136d25438e7c508d458de3f5f6d8e676
ShaCode256 <luzonshavit00@gmail.com>
2024-08-19 21:45:39 +0300

    Ptoviding options for http requests

--- src/app/wish.service.ts
+++ src/app/wish.service.ts
@@ -1,5 +1,6 @@
+import { WishItem } from './../shared/models/wishItem';
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
@@ -8,7 +9,30 @@ export class WishService {
 
   constructor(private http: HttpClient) { }
 
+  private getStandardOptions() : any {
+    return {
+      headers: new HttpHeaders({
+        'Content-Type': 'application/json',
+      })
+    };
+  }
+
   getWishes() {
-    return this.http.get('assets/wishes.json');
+    let options = this.getStandardOptions();
+
+    options.params = new HttpParams({
+      fromObject: {
+        format: 'json'
+      }
+    });
+
+    return this.http.get('assets/wishes.json', options);
+  }
+
+  private addWish(wish: WishItem) {
+    let options = this.getStandardOptions();
+
+    options.headers = options.headers.set('Authorization', 'value-need-for-authorization');
+    this.http.post('assets/wishes.json', wish, options);
   }
 }
